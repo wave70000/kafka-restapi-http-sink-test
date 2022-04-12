b51ccc715da065d377fe7d010f0f65789762add2
Wave70000 <wave70000@hotmail.com>
2022-04-12 16:03:34 +0700

    initial

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@PostMapping("/hello")
+	public String hello(@RequestBody String message) {
+		System.out.println(message);
+		return message;
+	}
+
+
+
+}
