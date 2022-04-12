791ceada4de1d77e1b9bb68c1b7fdabcd1b09153
Wave70000 <wave70000@hotmail.com>
2022-04-12 17:01:02 +0700

    changes v1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,25 +1,40 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+
+	ArrayList<String> record = new ArrayList<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@PostMapping("/hello")
-	public String hello(@RequestBody String message) {
+	@PostMapping("/api")
+	public String api(@RequestBody String message) {
+		record.add(message);
 		System.out.println(message);
 		return message;
 	}
 
+	@GetMapping("/clean")
+	public void clean() {
+		record.clear();
+	}
 
+	@GetMapping("/record")
+	public ArrayList<String> getRecord() {
+		return record;
+	}
 
 }
---
+++ src/main/java/com/example/demo/SimpleStringResponse.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class SimpleStringResponse {
+
+    private String message;
+
+    public SimpleStringResponse(String message) {
+        this.message = message;
+    }
+}
